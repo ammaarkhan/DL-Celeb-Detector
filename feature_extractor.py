09ae8b2e8fdff75044f964c20172bf449f7da6e4
ammaarkhan <ammaarkhan03@gmail.com>
2023-02-22 18:34:41 -0800

    implement feature extractor function

--- feature_extractor.py
+++ feature_extractor.py
@@ -15,18 +15,36 @@
 #         filenames.append(os.path.join('data', actor, imgFile))
 #
 # pickle.dump(filenames, open('filenames.pkl', 'wb'))
-
+import numpy as np
 from tensorflow.keras.preprocessing import image
 from keras_vggface.utils import preprocess_input
 from keras_vggface.vggface import VGGFace
 import numpy
 import pickle
 import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
+from tqdm import tqdm
 
 filenames = pickle.load(open('filenames.pkl', 'rb'))
 
-model = VGGFace(model='resnet50', include_top=False, input_shape=(244, 244, 3), pooling='avg')
+model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
 
-print(model.summary())
+
+# print(model.summary())
+
+def feature_extractor(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    expanded_img = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img)
+
+    result = model.predict(preprocessed_img).flatten()
+
+    return result
+
+
+features = []
+
+for file in tqdm(filenames):
+    res = feature_extractor(file, model)
+    print(res.shape)
+    break
